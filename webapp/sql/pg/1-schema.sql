c6a95d0005133b5ea463928d04006637b69cdde4
soudai <alftaketomo@gmail.com>
2024-12-08 11:17:24 +0900

    init.sh

--- webapp/sql/pg/1-schema.sql
+++ webapp/sql/pg/1-schema.sql
@@ -14,8 +14,8 @@ CREATE TABLE chair_models (
 
 DROP TABLE IF EXISTS chairs;
 CREATE TABLE chairs (
-    id UUID NOT NULL,                   -- 椅子ID
-    owner_id UUID NOT NULL,             -- オーナーID
+    id TEXT NOT NULL,                   -- 椅子ID
+    owner_id TEXT NOT NULL,             -- オーナーID
     name VARCHAR(30) NOT NULL,          -- 椅子の名前
     model TEXT NOT NULL,                -- 椅子のモデル
     is_active BOOLEAN NOT NULL,         -- 配椅子受付中かどうか
@@ -27,8 +27,8 @@ CREATE TABLE chairs (
 
 DROP TABLE IF EXISTS chair_locations;
 CREATE TABLE chair_locations (
-    id UUID NOT NULL,                   -- 主キー
-    chair_id UUID NOT NULL,             -- 椅子ID
+    id TEXT NOT NULL,                   -- 主キー
+    chair_id TEXT NOT NULL,             -- 椅子ID
     latitude INTEGER NOT NULL,          -- 経度
     longitude INTEGER NOT NULL,         -- 緯度
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, -- 登録日時
@@ -37,7 +37,7 @@ CREATE TABLE chair_locations (
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id UUID NOT NULL,                   -- ユーザーID
+    id TEXT NOT NULL,                   -- ユーザーID
     username VARCHAR(30) NOT NULL UNIQUE, -- ユーザー名
     firstname VARCHAR(30) NOT NULL,     -- 本名(名前)
     lastname VARCHAR(30) NOT NULL,      -- 本名(名字)
@@ -51,7 +51,7 @@ CREATE TABLE users (
 
 DROP TABLE IF EXISTS payment_tokens;
 CREATE TABLE payment_tokens (
-    user_id UUID NOT NULL,              -- ユーザーID
+    user_id TEXT NOT NULL,              -- ユーザーID
     token VARCHAR(255) NOT NULL,        -- 決済トークン
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, -- 登録日時
     PRIMARY KEY (user_id)
@@ -59,9 +59,9 @@ CREATE TABLE payment_tokens (
 
 DROP TABLE IF EXISTS rides;
 CREATE TABLE rides (
-    id UUID NOT NULL,                   -- ライドID
-    user_id UUID NOT NULL,              -- ユーザーID
-    chair_id UUID,                      -- 割り当てられた椅子ID
+    id TEXT NOT NULL,                   -- ライドID
+    user_id TEXT NOT NULL,              -- ユーザーID
+    chair_id TEXT,                      -- 割り当てられた椅子ID
     pickup_latitude INTEGER NOT NULL,   -- 配車位置(経度)
     pickup_longitude INTEGER NOT NULL,  -- 配車位置(緯度)
     destination_latitude INTEGER NOT NULL, -- 目的地(経度)
@@ -74,8 +74,8 @@ CREATE TABLE rides (
 
 DROP TABLE IF EXISTS ride_statuses;
 CREATE TABLE ride_statuses (
-    id UUID NOT NULL,                   -- 主キー
-    ride_id UUID NOT NULL,              -- ライドID
+    id TEXT NOT NULL,                   -- 主キー
+    ride_id TEXT NOT NULL,              -- ライドID
     status VARCHAR(20) CHECK (status IN ('MATCHING', 'ENROUTE', 'PICKUP', 'CARRYING', 'ARRIVED', 'COMPLETED')) NOT NULL, -- 状態
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, -- 状態変更日時
     app_sent_at TIMESTAMP WITH TIME ZONE, -- ユーザーへの状態通知日時
@@ -85,7 +85,7 @@ CREATE TABLE ride_statuses (
 
 DROP TABLE IF EXISTS owners;
 CREATE TABLE owners (
-    id UUID NOT NULL,                   -- オーナーID
+    id TEXT NOT NULL,                   -- オーナーID
     name VARCHAR(30) NOT NULL UNIQUE,   -- オーナー名
     access_token VARCHAR(255) NOT NULL UNIQUE, -- アクセストークン
     chair_register_token VARCHAR(255) NOT NULL UNIQUE, -- 椅子登録トークン
@@ -96,10 +96,10 @@ CREATE TABLE owners (
 
 DROP TABLE IF EXISTS coupons;
 CREATE TABLE coupons (
-    user_id UUID NOT NULL,              -- 所有しているユーザーのID
+    user_id TEXT NOT NULL,              -- 所有しているユーザーのID
     code VARCHAR(255) NOT NULL,         -- クーポンコード
     discount INTEGER NOT NULL,          -- 割引額
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, -- 付与日時
-    used_by UUID,                       -- クーポンが適用されたライドのID
+    used_by TEXT,                       -- クーポンが適用されたライドのID
     PRIMARY KEY (user_id, code)
 );
