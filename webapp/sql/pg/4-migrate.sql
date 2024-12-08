e6c2b261d01cbabd0cf6b487ad57a96a4551738c
mackee <macopy123@gmail.com>
2024-12-08 17:37:30 +0900

    Revert "Feature/total distance"

--- webapp/sql/pg/4-migrate.sql
+++ webapp/sql/pg/4-migrate.sql
@@ -6,18 +6,3 @@ DELETE FROM vacant_chair WHERE chair_id IN (
   INNER JOIN ride_statuses ON rides.id = ride_statuses.ride_id
   WHERE chair_sent_at IS NULL
 );
-
-INSERT INTO chair_locations_summary (chair_id, total_distance, total_distance_updated_at)
-  SELECT id AS chair_id,
-         COALESCE(total_distance, 0) AS total_distance,
-         total_distance_updated_at AS total_distance_updated_at
-  FROM chairs
-         LEFT JOIN (SELECT chair_id,
-                            SUM(COALESCE(distance, 0)) AS total_distance,
-                            MAX(created_at)          AS total_distance_updated_at
-                     FROM (SELECT chair_id,
-                                  created_at,
-                                  ABS(latitude - LAG(latitude) OVER (PARTITION BY chair_id ORDER BY created_at)) +
-                                 ABS(longitude - LAG(longitude) OVER (PARTITION BY chair_id ORDER BY created_at)) AS distance
-                           FROM chair_locations) tmp
-                     GROUP BY chair_id) distance_table ON distance_table.chair_id = chairs.id;
